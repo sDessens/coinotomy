a34e3029b6b48894adf440e485db72b0140cae69
Stefan Dessens <Stefan.Dessens@gmail.com>
2017-01-18 22:03:38 +0100

    Change chbtc from websocket to rest to fight great firewall issues

--- coinotomy/watchers/chbtc.py
+++ coinotomy/watchers/chbtc.py
@@ -1,17 +1,15 @@
 import json
-import logging
-import asyncio
-import websockets
-import time
+import urllib.request
 
 from coinotomy.watchers.common import Watcher
 
 
 class WatcherChbtc(Watcher):
-    def __init__(self, pair):
-        self.pair = pair
-        self.name = 'chbtc.' + pair
-        self.log = logging.getLogger(self.name + pair)
+    def __init__(self, name):
+        Watcher.__init__(self, "chbtc." + name, 10)
+
+        self.backend = None
+        self.api = ChbtcApi(name, self.log)
 
     def __del__(self):
         self.unload()
@@ -19,51 +17,77 @@ class WatcherChbtc(Watcher):
     def name(self):
         return self.name
 
-    def run(self, backend):
-        loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop)
-        while True:
-            try:
-                loop.run_until_complete(self.connect(backend))
-            except Exception:
-                logging.error('retry')
-                time.sleep(5)
+    def setup(self, backend):
+        self.backend = backend
 
-    @asyncio.coroutine
-    def connect(self, backend):
-        newest_tid = 0
-        newest_timestamp = 0
-        for ts, price, vol in backend.rlines():
-            newest_timestamp = ts
+        # find the last trade in the storage backend
+        last_trade = None
+        for trade in self.backend.rlines():
+            last_trade = trade
             break
 
-        websocket = yield from websockets.connect('wss://api.chbtc.com:9999/websocket')
+        # determine the timestamp of the last trade
+        if last_trade is None:
+            self.newest_timestamp = 0
+            self.newest_tid = 0
+        else:
+            self.newest_timestamp = last_trade[0] + 0.0001
+            self.newest_tid = 0
 
-        subscription_channel = self.pair + '_trades'
+    def tick(self):
+        if self.newest_tid:
+            # trades filtered by api
+            trades, self.newest_tid = self.api.more(self.newest_tid)
+        else:
+            # Don't know newest TID. filter trades based on timestamp
+            trades, self.newest_tid = self.api.more()
+            trades = list(filter(lambda row: row[0] >= self.newest_timestamp, trades))
 
-        # subscribe to pair
-        yield from websocket.send(str({'event': 'addChannel', 'channel': subscription_channel}))
+        for ts, p, v in trades:
+            self.backend.append(ts, p, v)
+        self.backend.flush()
 
-        while True:
-            message = yield from websocket.recv()
-            js = json.loads(message)
-            channel = js['channel']
+    def unload(self):
+        if self.backend:
+            self.backend.unload()
+        self.backend = None
 
-            if channel != subscription_channel:
-                self.log.warn('unknown channel {}'.format(channel))
 
-            for line in js['data']:
-                timestamp = line['date']
-                amount = line['amount']
-                price = line['price']
-                tid = line['tid']
-                if newest_tid == 0 and timestamp <= newest_timestamp:
-                    continue
-                if newest_tid >= tid:
-                    continue
-                newest_tid = tid
-                backend.append(timestamp, price, amount)
-            backend.flush()
+class ChbtcApi(object):
+    def __init__(self, symbol, log):
+        self.symbol = symbol
+        self.log = log
+
+    def more(self, since_tid=0):
+        return self._parse_response(self._query(), since_tid=since_tid)
+
+    def _query(self, since_tid=0):
+        url = 'http://api.chbtc.com/data/v1/trades?currency=%s/&since=%i' % \
+            (self.symbol, since_tid)
+        with urllib.request.urlopen(url, timeout=10) as response:
+            return str(response.read(), 'ascii')
+
+    def _parse_response(self, html, since_tid=0):
+        """
+        return (array_of_trades, newest_tid)
+        """
+        js = json.loads(html)
+        trades = []
+        newest_tid = since_tid
+        for row in js:
+            tid = int(row['tid'])
+            if tid <= since_tid:
+                continue
+
+            newest_tid = max(newest_tid, tid)
+            timestamp = float(row['date'])
+            price = float(row['price'])
+            amount = float(row['amount'])
+            trades.append((timestamp, price, amount))
+
+        # sort just in case
+        return sorted(trades, key=lambda x: x[0]), newest_tid
+
 
 watchers = [
     WatcherChbtc("btc_cny"),
